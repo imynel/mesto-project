90f5fffb7520c2c260de98bff6952b23d14bad78
imynel <nsmayorov@gmail.com>
2022-12-18 01:43:04 +0300

    editig profile name and job

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
+// Для открытия и закрытия попапа
 let profileButton = document.querySelector('.profile__button') // для открытия кнопки редактирования
 let addCardButton = document.querySelector('.profile__add-button') // для открытия попапа добавления карточек
 let popupProfile = document.querySelector('.popup-profile')
 let popupCards = document.querySelector('.popup-cards')
-let closeButton = document.querySelectorAll('.popup__close-image')
-console.log(closeButton)
+let closeButtonProfile = popupProfile.querySelector('.popup__close-image')
+let closeButtonCards = popupCards.querySelector('.popup__close-image')
+// Редактирование имени и информации о себе
+const formElementProfile = popupProfile.querySelector('.popup__form')
+const nameInput = formElementProfile.querySelector('.name_input')
+const jobInput = formElementProfile.querySelector('.job_input')
+const formElementCards = popupCards.querySelector('.popup__form')
+const placInpur = formElementCards.querySelector('.name_place')
+const linkInput = formElementCards.querySelector('.name_link')
+const nameProfile = document.querySelector('.profile__name')
+const jobProfile = document.querySelector('.profile__text')
 
 function openedPopup(popup) {
   popup.classList.add('popup_opened')
@@ -21,39 +31,21 @@ addCardButton.addEventListener('click', () => {
   openedPopup(popupCards)
 })
 
-closeButton[0].addEventListener('click', () => {
+closeButtonProfile.addEventListener('click', () => {
   closedPopup(popupProfile)
 })
 
-closeButton[1].addEventListener('click', () => {
+closeButtonCards.addEventListener('click', () => {
   closedPopup(popupCards)
 })
 
-// // открытие popup-profile
-// function openedPopupProfile() {
-//   return popup[0].classList.add('popup_opened')
-// }
+function handleFormSubmit(evt) {
+  evt.preventDefault()
 
-// profileButton.addEventListener('click', openedPopupProfile)
+  nameProfile.textContent = nameInput.value
+  jobProfile.textContent = jobInput.value
 
-// // закрытие popup-profile
-// function closedPopupProfile() {
-//   return popup[0].classList.remove('popup_opened')
-// }
-
-// popupClose[0].addEventListener('click', closedPopupProfile)
-
-// // открытие popup-cards
-// function openedPopupCards() {
-//   return popup[1].classList.add('popup_opened')
-// }
-
-// addCardButton.addEventListener('click', openedPopupCards)
-
-// // закрытие popup-cards
-// function closedPopupCards() {
-//   return popup[1].classList.remove('popup_opened')
-// }
-
-// popupClose[1].addEventListener('click', closedPopupCards)
-
+  closedPopup(popupProfile)
+}
+formElementProfile.addEventListener('submit', handleFormSubmit)
+// formElementCards.addEventListener('submit', handleFormSubmit)
