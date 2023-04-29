b08ddca79451fedcc70f0351a2a05b9364948163
Nikita Mayorov <nsmayorov@gmail.com>
2023-04-29 19:30:20 +0300

    add fich

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,10 +1,10 @@
 import Api from "./classApi.js"
 // Создание класса UserInfo
-export default class UserInfo extends Api{
-  constructor(name, about, options){
-    super(options)
-    this._name = name
-    this._about = about
+export default class UserInfo{
+  constructor(name, about, avatar){
+    this.name = name
+    this.about = about
+    this.avatar = avatar
   }
 
   getUserInfo() {
@@ -16,10 +16,11 @@ export default class UserInfo extends Api{
     //   })
   }
 
-  setUserInfo(nameValue, aboutValue) {
-    this.patchRequestPrifile(nameValue, aboutValue)
-    this._name.textContent = nameValue
-    this._about.textContent = aboutValue
+  setUserInfo(data) {
+    if (data.name) this.name.textContent = data.name
+    if (data.about) this.about.textContent = data.about
+    if (data.avatar) this.avatar.src = data.avatar
+    if (data._id) this.userId = data._id
   }
 }
 
